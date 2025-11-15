77f9b7cd8d2e627152da14261ea5a1f478ef18e2
Zux2811 <npavu2811@gmail.com>
2025-11-15 09:57:58 +0700

    fix: add ssl config for sequelize railway

--- src/config/db.js
+++ src/config/db.js
@@ -6,11 +6,18 @@ dotenv.config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
