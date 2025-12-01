82e9e00101d0d7f90e099789797ac12bae308e23
Zux2811 <npavu2811@gmail.com>
2025-12-01 19:58:39 +0700

    fix 500 library

--- src/models/folder.model.js
+++ src/models/folder.model.js
@@ -4,10 +4,7 @@ import sequelize from "../config/db.js";
 const Folder = sequelize.define("Folder", {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   name: { type: DataTypes.STRING, allowNull: false },
-  UserId: { type: DataTypes.INTEGER, allowNull: false }, // ✅ Foreign key to User
-}, {
-  tableName: "folders",
-  timestamps: true,
+  UserId: { type: DataTypes.INTEGER, allowNull: false }, // Foreign key to User
 });
 
 export default Folder;
